0a4f71785d80f3bc66f529e9ff320f3687c3ea12
Suhas Lucia <42386147+suhaslucia@users.noreply.github.com>
2020-12-27 09:01:20 +0530

    Height as Perpendicular

--- Pythagoras Theorem.py
+++ Pythagoras Theorem.py
@@ -1,26 +1,27 @@
 from math import sqrt
 
 print(" ----------Pythagoras Theorem to Find the sides of the Triangle---------- ")
+print(" ------------ Enter any one side as 0 to obtain the result -------------- ")
 
 base = int(input("Enter the base of the Triangle: "))
-height = int(input("Enter the height of the Triangle: "))
+perpendicular = int(input("Enter the perpendicular of the Triangle: "))
 hypotenuse = int(input("Enter the hypotenuse of the Triangle: "))
 
 if hypotenuse == 0:
     b = base ** 2
-    h = height ** 2
-    hyp = b + h
+    p = perpendicular ** 2
+    hyp = b + p
     result = sqrt(hyp)
     print(f'\n Hypotenuse of Triangle is {result}')
-elif height == 0:
+elif perpendicular == 0:
     b = base ** 2
     hyp = hypotenuse ** 2
-    h = hyp - b
-    result = sqrt(h)
-    print(f'\n Height of Triangle is {result}')
+    p = hyp - b
+    result = sqrt(p)
+    print(f'\n Perpendicular of Triangle is {result}')
 else:
-    h = height ** 2
+    p = perpendicular ** 2
     hyp = hypotenuse ** 2
-    b = hyp - h
+    b = hyp - p
     result = sqrt(b)
     print(f'\n Base of Triangle is {result}')
